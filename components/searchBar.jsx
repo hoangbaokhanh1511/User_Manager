b35c2d897d0247b3d364c1840768445340ece378
hoangbaokhanh1511 <22t1020637@husc.edu.vn>
2025-10-21 00:17:56 +0700

    [Update code searchBar]

--- components/searchBar.jsx
+++ components/searchBar.jsx
@@ -1,17 +1,4 @@
-// "use client";
-// export default function Header() {
-//     return (
-//         <header style={{
-//             backgroundColor: "#0070f3",
-//             padding: "16px",
-//             color: "white", 
-//             textAlign: "center"
-//         }}>
-//             <h1> MINI NEXT.JS APP </h1>
-//             <p> EXAMPLES OF USING COMPONENTS</p>
-//         </header>
-//     );
-// }
+
 
 // Component: Tìm kiếm người dùng
 "use client"
